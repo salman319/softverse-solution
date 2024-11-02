090bc358925bb6ed7daa63c49c7a01310e098d26
M Salman Saleem <msalmansaleem1998@gmail.com>
2024-11-02 21:38:17 +0500

    softverse site

---
+++ scripit.js
@@ -0,0 +1,69 @@
+//menu section start
+document.querySelector(".menu svg").addEventListener('click', () => {
+    if (document.querySelector(".sidebar-section").style.height === "0px") {
+        document.querySelector(".sidebar-section").style.height = "200px"
+    } else {
+        document.querySelector(".sidebar-section").style.height = "0px"
+    }
+})
+//menu section end
+//slider section start
+
+$(document).ready(function () {
+
+    var owl = $('.owl-carousel-1').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+
+
+    $('.left').on('click', function () {
+        owl.trigger('prev.owl.carousel');
+    });
+
+    $('.right').on('click', function () {
+        owl.trigger('next.owl.carousel');
+    });
+});
+$(document).ready(function () {
+    var owlCarousel2 = $(".owl-carousel-2").owlCarousel({
+        loop: true,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+
+
+    $(".left-2").click(function () {
+        owlCarousel2.trigger('prev.owl.carousel');
+    });
+
+
+    $(".right-2").click(function () {
+        owlCarousel2.trigger('next.owl.carousel');
+    });
+});
+
+//slider section end
+
